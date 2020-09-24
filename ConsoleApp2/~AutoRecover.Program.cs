8e772b6a8a3c82bcdb6550a3652b209ae4fd9242
sakeena81 <sakeena77a81@gmail.com>
2020-09-24 22:09:13 +0100

    ren&stimpy

---
+++ ConsoleApp2/~AutoRecover.Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stimpy T = new Stimpy();
+            T. 
+            T.Add(2, 3);
+            T.Subtraction(2, 3);
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/~AutoRecover.Stimpy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Stimpy:Ren
+    {
+        public void Subtraction(int r, int s)
+        {
+            int result = r - s;
+            Console.Write("The result of subtraction for Ren is :");{
+}
+       
+
+        
+
+        }
+    }
+}
